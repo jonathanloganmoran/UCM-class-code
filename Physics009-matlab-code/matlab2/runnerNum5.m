5229d1a78d6e27e5f1c29d9611345faa29dc5cf1
Miguel Hernandez <mhernandez401405@gmail.com>
2016-10-26 18:39:50 -0700

    Update last question

--- Physics009-matlab-code/matlab2/runnerNum5.m
+++ Physics009-matlab-code/matlab2/runnerNum5.m
@@ -1,7 +1,16 @@
 %Miguel Hernandez
 %Physics 009H
 %5. Using the function you wrote in (1), 
-%?nd the integral of the function 
-%y(x) = ex, between x0 = 0 and xn = 25. Choose an appropriate 
-%?x so that your result is close to the actual value of the integral 
-%(You will have to ?nd the value of the integral using your knowledge of calculus).
+%and the integral of the function 
+%y(x) = e^x, between x0 = 0 and xn = 25. Choose an appropriate 
+%dx so that your result is close to the actual value of the integral 
+%(You will have to find the value of the integral using your knowledge of calculus).
+
+dx = .001;
+x = 0:dx:25;
+
+y = exp^(x);
+
+areaOfExp = trapezoidIntegral(x, y)
+
+
